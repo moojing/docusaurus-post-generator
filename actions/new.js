04c1134dff5a10d137060517c5d7f3ede4703c77
moojing <starts789@gmail.com>
2022-08-13 17:33:17 +0800

    [Feat] Finish replacing the title and slug with template file.

--- actions/new.js
+++ actions/new.js
@@ -3,37 +3,61 @@
 // include node fs module
 var fs = require('fs')
 const dayjs = require('dayjs')
+const readline = require('node:readline');
 
-module.exports = (template, title) => {
-  console.log('template :', template);
+const getTemplateFilePath = (name) => 'template/' + name + '.md'
+
+async function getTemplateContent(templatePath, title) {
+  let templateContent = ''
+  templateReader = fs.createReadStream(templatePath, { encoding: 'utf8', });
+  try {
+    const rl = readline.createInterface({
+      input: templateReader,
+      // crlfDelay: Infinity
+    });
+
+    for await (const line of rl) {
+      let tempLine = line
+
+      // replace the title value with the title that user provided.
+      if(line.includes('title:')) {
+        tempLine = `title: ${title}`
+      }else if (line.includes('slug:')){
+        tempLine = `slug: ${title.split(' ').join('-').toLowerCase()}`
+      }
+      templateContent += tempLine + '\n'
+    }
+  } catch (err) {
+    console.log('An error happened while reading the file.', err);
+  }
+
+  return templateContent
+
+}
+
+module.exports = async (template, title) => {
+
   const date = dayjs().format('YYYY-MM-DD')
 
-  const getTemplateFile = (name) => 'template/' + name + '.md'
-  const fileName = `${date}-${title}`;
+  const fileName = `${date}-${title.split(' ').join('-')}`;
   let templateContent = '';
   let isDefaultTemplate = false;
 
-  if (fs.existsSync(getTemplateFile(template))) {
-    try {
-      templateContent = fs.readFileSync(
-        getTemplateFile(template), 'utf8'
-      );
-
-      console.log(data);
-    } catch (err) {
-      console.error('An error happen while reading file: ', err);
-    }
+  if (fs.existsSync(getTemplateFilePath(template))) {
+    templateContent = await getTemplateContent(getTemplateFilePath(template), title)
   } else {
     isDefaultTemplate = true;
-    templateContent = fs.readFileSync(getTemplateFile('default'), 'utf8');
+    templateContent = await getTemplateContent(getTemplateFilePath('default'), title)
   }
+
+
 
   fs.writeFile(`${fileName}.md`, templateContent, function (err) {
     if (err) throw err;
     let createdMessage = 'Created a new post: ' + fileName + '.md'
-    if(isDefaultTemplate){
+    if (isDefaultTemplate) {
       createdMessage += ' with the default template. \nYou can change the template by adding the template folder.'
-    } 
+    }
     console.log(createdMessage);
   });
 
